d11b1eb01c613a177f1995c8152e9e1f691ffe7a
nk-knvlv <nk_knvlv@mail.ru>
2024-08-12 20:39:11 +0300

    update; add token

---
+++ app.py
@@ -0,0 +1,12 @@
+import telebot
+
+# Замените на ваш токен API
+TOKEN = "7119506745:AAF_Sn9rDnkCYh3i23WWjaKBVQ2ldMDdL-s"
+
+bot = telebot.TeleBot(TOKEN)
+
+@bot.message_handler(func=lambda message: True)
+def echo_message(message):
+    bot.reply_to(message, message.text)
+
+bot.polling()
